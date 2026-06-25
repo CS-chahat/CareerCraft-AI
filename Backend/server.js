5c9cc0ef25879236023e92d057bf1c2370706aaf
chahat <chahatshishodia72@gmail.com>
2026-06-25 16:57:42 +0530

    fix: express import in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,20 @@
 require("dotenv").config()
+const express = require("express")
 const app = require("./src/app")
 const connectToDB = require("./src/config/database.js")
 
 
 // 1. Connect to your database
 connectToDB()
+const path = require('path');
+
+// Serve static files from the Frontend dist directory
+app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+
+// Handle any requests that don't match your backend API routes by sending back the index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Frontend/dist', 'index.html'));
+});
 
 // 3. Start your network port listener
 app.listen(3000, () => {
